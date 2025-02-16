e1c8bfd657a7d4d69d020d51c9228d42a818792e
mahmoud.maghrabia@vodafone.com <mahmoud.maghrabia@vodafone.com>
2025-02-16 13:56:37 +0200

    feat: prep to prod

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import ErrorBoundary from "./components/ErrorBoundary";
 import Index from "./pages/Index";
 import Settings from "./pages/Settings";
@@ -12,23 +12,25 @@ import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
+
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <ErrorBoundary>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <Router> {/* Change BrowserRouter to HashRouter */}
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/settings" element={<Settings />} />
             <Route path="/statistics" element={<Statistics />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </Router>
       </ErrorBoundary>
     </TooltipProvider>
   </QueryClientProvider>
 );
+
 
 export default App;
