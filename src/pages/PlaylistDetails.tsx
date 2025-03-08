73f3a79f66de3230f6e75f700369b5fe0c100fda
mahmoud.maghrabia@vodafone.com <mahmoud.maghrabia@vodafone.com>
2025-03-08 21:30:49 +0200

    use id for playlists

--- src/pages/PlaylistDetails.tsx
+++ src/pages/PlaylistDetails.tsx
@@ -8,6 +8,8 @@ import { Header } from "@/components/Header";
 import { useVideoStore } from "@/store/videoStore";
 import { toast } from "sonner";
 
+const ID_COUNTER = `1312921/t/5`;
+
 export default function PlaylistDetails() {
   const { playlistId } = useParams<{ playlistId: string }>();
   const [searchParams] = useSearchParams();
@@ -30,6 +32,28 @@ export default function PlaylistDetails() {
       console.error("Error loading playlist videos:", videosError);
     }
   }, [videosError]);
+
+  useEffect(() => {
+    fetch(`https://www.freevisitorcounters.com/en/home/counter/${ID_COUNTER}`, {
+      "headers": {
+        "accept": "*/*",
+        "accept-language": "en-US,en;q=0.9",
+        "priority": "u=1",
+        "sec-ch-ua": "\"Chromium\";v=\"134\", \"Not:A-Brand\";v=\"24\", \"Google Chrome\";v=\"134\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"macOS\"",
+        "sec-fetch-dest": "script",
+        "sec-fetch-mode": "no-cors",
+        "sec-fetch-site": "cross-site",
+        "sec-fetch-storage-access": "active"
+      },
+      "referrerPolicy": "strict-origin-when-cross-origin",
+      "body": null,
+      "method": "GET",
+      "mode": "cors",
+      "credentials": "omit"
+    });
+  }, []);
 
   const handleProgressChange = (videoId: string, seconds: number, duration: number) => {
     console.log('handleProgressChange', videoId, duration);
@@ -80,7 +104,7 @@ export default function PlaylistDetails() {
   return (
     <div className="min-h-screen bg-background">
       <Header />
-      <div className="container mx-auto px-4">
+      <div className="container mx-auto px-4 flex flex-col items-center justify-center" style={{ height: 'calc(100vh - 73px)' }}>
         <PlaylistView
           playlist={completePlaylist}
           selectedVideoId={selectedVideoId}
@@ -90,6 +114,13 @@ export default function PlaylistDetails() {
           onProgressChange={handleProgressChange}
         />
       </div>
+      {/* <div className="flex items-center justify-center" style={{ height: '100px' }}>
+      <img 
+          src={`https://www.freevisitorcounters.com/en/counter/render/${ID_COUNTER}`} 
+          alt="counter"
+          className="mt-4 w-auto"
+        />
+      </div> */}
     </div>
   );
 }
